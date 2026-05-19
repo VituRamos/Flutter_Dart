e3c8b4cc512bbf0686874b10397dd0b3ddd89368
Victor Ramos <victor.ramosbernardes@gmail.com>
2026-05-19 12:49:06 -0300

    Remove portfolio_2 Flutter project

--- Projetos/portfolio_2/lib/utils/app_routes.dart
+++
@@ -1,6 +0,0 @@
-class AppRoutes {
-  static const IntroPage = '/intro_page';
-  static const ShopPage = '/shop_page';
-  static const CartPage = '/cart_page';
-  static const FavoritePage = '/favorite_page';
-}
